d0b297209ab5cd54cc6f8a99c9064199b30ce7da
Khawaja Fakhar <khawajafakhar972@gmail.com>
2025-06-03 00:29:05 +0500

    Added: Product display screen

---
+++ lib/screens/product_list/product_list_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../providers/product_provider.dart';
+
+class ProductListScreen extends StatefulWidget {
+  @override
+  State<ProductListScreen> createState() => _ProductListScreenState();
+}
+
+class _ProductListScreenState extends State<ProductListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Products")),
+      body: ChangeNotifierProvider(
+        create: (context) => ProductProvider()..loadProducts(),
+        child: Consumer<ProductProvider>(
+          builder: (context, provider, child) {
+            if (provider.isLoading) {
+              return Center(child: CircularProgressIndicator());
+            }
+            if (provider.products.isEmpty) {
+              return const Center(
+                  child: Text("No products available"));
+            }
+            return RefreshIndicator(
+              onRefresh: ()async {
+                context.read<ProductProvider>().loadProducts();
+              },
+              child: ListView.builder(
+                itemCount: provider.products.length,
+                itemBuilder: (context, index) {
+                  
+                  final product = provider.products[index];
+                  return ListTile(
+                    leading: product.thumbnail != null
+                        ? Image.network(product.thumbnail!, width: 50)
+                        : const SizedBox(),
+                    title: Text(product.title ?? "(No Title Product)"),
+                    subtitle: Text("\$${product.price} • ${product.brand ?? ""}"),
+                    trailing: Text(
+                        (product.stock ?? 0) > 0 ? "In Stock" : "Out of Stock"),
+                 
+                  );
+                },
+              ),
+            );
+          },
+        ),
+      ),
+     
+    );
+  }
+}
